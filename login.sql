97ed67239f5949b3987788a2440238b63c0ce1d2
[firstname lastname] <[email]>
2022-12-02 23:46:09 +0700

    10nd commit

--- login.sql
+++ login.sql
@@ -25,10 +25,15 @@ DROP TABLE IF EXISTS `cart`;
 CREATE TABLE `cart` (
   `productid` int NOT NULL,
   `quantity` int NOT NULL,
+  `cartid` int NOT NULL AUTO_INCREMENT,
+  `orderid` int NOT NULL,
   `userid` int NOT NULL,
-  PRIMARY KEY (`userid`),
-  CONSTRAINT `fk_user_id1` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`cartid`),
+  KEY `fk_product_id1` (`productid`),
+  KEY `fk_cart_orders1_idx` (`orderid`,`userid`),
+  CONSTRAINT `fk_cart_orders1` FOREIGN KEY (`orderid`) REFERENCES `orders` (`orderid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_product_id1` FOREIGN KEY (`productid`) REFERENCES `products` (`productid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +42,7 @@ CREATE TABLE `cart` (
 
 LOCK TABLES `cart` WRITE;
 /*!40000 ALTER TABLE `cart` DISABLE KEYS */;
-INSERT INTO `cart` VALUES (4,1,5);
+INSERT INTO `cart` VALUES (12,2,12,9,8),(11,1,13,9,8),(10,1,14,9,8);
 /*!40000 ALTER TABLE `cart` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,7 +57,7 @@ CREATE TABLE `categories` (
   `categoryid` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   PRIMARY KEY (`categoryid`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -61,36 +66,8 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (1,'Meat'),(2,'Dairy');
+INSERT INTO `categories` VALUES (5,'Meat'),(6,'Dairy');
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `orderdetails`
---
-
-DROP TABLE IF EXISTS `orderdetails`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `orderdetails` (
-  `orderid` int NOT NULL,
-  `productid` int NOT NULL,
-  `quantity` int NOT NULL,
-  `priceeach` double NOT NULL,
-  PRIMARY KEY (`orderid`,`productid`),
-  KEY `fk_orderdetails_product1_idx` (`productid`),
-  CONSTRAINT `fk_orderdetails_order1` FOREIGN KEY (`orderid`) REFERENCES `orders` (`orderid`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_orderdetails_product1` FOREIGN KEY (`productid`) REFERENCES `products` (`productid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `orderdetails`
---
-
-LOCK TABLES `orderdetails` WRITE;
-/*!40000 ALTER TABLE `orderdetails` DISABLE KEYS */;
-/*!40000 ALTER TABLE `orderdetails` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -101,12 +78,12 @@ DROP TABLE IF EXISTS `orders`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `orders` (
-  `orderid` int NOT NULL AUTO_INCREMENT,
+  `orderid` int NOT NULL,
   `userid` int NOT NULL,
   `orderdate` date NOT NULL,
-  PRIMARY KEY (`orderid`,`userid`),
-  KEY `fk_order_customer_idx` (`userid`),
-  CONSTRAINT `fk_order_customer` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE
+  PRIMARY KEY (`orderid`),
+  KEY `fk_orders_users1_idx` (`userid`),
+  CONSTRAINT `fk_orders_users1` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -116,6 +93,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (0,8,'2022-12-02'),(1,8,'2022-12-02'),(2,8,'2022-12-02'),(3,8,'2022-12-02'),(4,8,'2022-12-02'),(5,8,'2022-12-02'),(6,8,'2022-12-02'),(7,8,'2022-12-02'),(8,8,'2022-12-02');
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -137,7 +115,7 @@ CREATE TABLE `products` (
   PRIMARY KEY (`productid`,`categoryid`),
   KEY `fk_products_categories1` (`categoryid`),
   CONSTRAINT `fk_products_categories1` FOREIGN KEY (`categoryid`) REFERENCES `categories` (`categoryid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -146,7 +124,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (3,1,'Beef 1kg','2022-12-12',4.99,10,0),(4,2,'Milk','2022-12-12',1.99,10,0),(5,1,'Beef 2kg','2022-12-12',9.99,20,0),(6,1,'Beef 3kg','2022-12-12',14.99,10,0),(7,1,'Beef 4kg','2022-12-12',19.99,20,0);
+INSERT INTO `products` VALUES (10,5,'Beef 1kg','2022-12-12',4.99,10,0),(11,5,'Beef 2kg','2022-12-12',9.99,10,0),(12,5,'Beef 3kg','2022-12-12',14.99,10,0);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -165,7 +143,7 @@ CREATE TABLE `users` (
   `admin` int NOT NULL,
   `salt` varchar(45) NOT NULL,
   PRIMARY KEY (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -174,7 +152,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (5,'tester','zUtz+lcXe4c2yFV1SeKeY2CL9RNkRxN2Kctkijqx1bc=','tester@gmail.com',0,'kFeThVMhQyJzfZ4S4T3Z'),(6,'admin','plVVZOGjXWA3jBx07B4nr9DKiosIYOZC+oeFdTVgqWs=','admin@gmail.com',1,'M4IycCz9ijYTgct2xhQg'),(7,'tester01','KUOkOtvHaYnNzq1mjF7D7vt08pc29OmviczWh6I7d0c=','tester01@gmail.com',0,'fuWtgfMJwxptyCyQbVos');
+INSERT INTO `users` VALUES (8,'tester','GIVxN695i5Sn3os4nkDOnSFCfOb4Va79qqvtu3Lph9s=','tester@gmail.com',0,'KZlYHgXr076QRRtfufLV'),(9,'admin','62zYLiisaqHJFiF+O0rIP2zi7yQHRXTGgkioKRJorQQ=','admin@gmail.com',1,'F4jaVnhQdOW7nRt9gGoS');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -187,4 +165,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-27 21:40:04
+-- Dump completed on 2022-12-02 23:45:41
